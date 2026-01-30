ccea9e34df57f9f51d7d01ecd6f66a76b64b189d
umport <tk54vptpwy@privaterelay.appleid.com>
2026-01-30 07:01:32 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = withPWA({
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+});
+
+module.exports = nextConfig;
